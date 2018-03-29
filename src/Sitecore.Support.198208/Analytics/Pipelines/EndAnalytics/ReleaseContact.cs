24e72cef41e8afec2bc4f217b44f8fce0fdcd099
DK\DTAT <dtat@sitecore.net>
2018-03-29 09:17:13 +0300

    Initial commit

---
+++ src/Sitecore.Support.198208/Analytics/Pipelines/EndAnalytics/ReleaseContact.cs
@@ -0,0 +1,52 @@
+﻿namespace Sitecore.Support.Analytics.Pipelines.EndAnalytics
+{
+    using Sitecore.Analytics;
+    using Sitecore.Analytics.Tracking;
+    using Sitecore.Configuration;
+    using Sitecore.Diagnostics;
+    using Sitecore.Pipelines;
+    using System;
+    using System.Reflection;
+
+    public class ReleaseContact
+    {
+        public void Process(PipelineArgs args)
+        {
+            if (Tracker.Current == null)
+            {
+                Log.Debug("Tracker is not initialized. ReleaseContact processor is skipped");
+                return;
+            }
+            Session session = Tracker.Current.Session;
+            Assert.IsNotNull(session, "Tracker.Current.Session");
+
+            bool transferInProgress =
+              (bool)session.GetType()
+                .GetProperty("TransferInProcess", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetProperty)
+                .GetValue(session);
+
+            if (transferInProgress)
+            {
+                Log.Debug("Contact is being transferred. ReleaseContact processor is skipped");
+                return;
+            }
+
+            if (session.Contact == null)
+            {
+                Log.Debug("Contact is null. ReleaseContact processor is skipped");
+                return;
+            }
+
+            ContactManager contactManager = Factory.CreateObject("tracking/contactManager", true) as ContactManager;
+            Assert.IsNotNull(contactManager, "tracking/contactManager");
+            if (session.Settings.IsTransient)
+            {
+                Log.Debug("Session is in TRANSIENT MODE. ReleaseContact processor is skipped");
+                return;
+            }
+
+            contactManager.SaveAndReleaseContact(session.Contact);
+            session.Contact = null;
+        }
+    }
+}
---
+++ src/Sitecore.Support.198208/Analytics/Pipelines/EnsureSessionContext/SetDummySession.cs
@@ -0,0 +1,28 @@
+﻿namespace Sitecore.Support.Analytics.Pipelines.EnsureSessionContext
+{
+    using Sitecore.Analytics.Pipelines.InitializeTracker;
+    using Sitecore.Analytics.Tracking;
+
+    public class SetDummySession : InitializeTrackerProcessor
+    {
+        public int MaxPageIndexThreshold
+        {
+            get;
+            set;
+        }
+
+        public override void Process(InitializeTrackerArgs args)
+        {
+            if (args.Session != null && args.Session.Interaction != null && args.Session.Interaction.PageCount >= this.MaxPageIndexThreshold)
+            {
+                System.IO.MemoryStream memoryStream = new System.IO.MemoryStream();
+                System.Runtime.Serialization.Formatters.Binary.BinaryFormatter binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(memoryStream, args.Session);
+                memoryStream.Position = 0L;
+                args.Session = (Session)binaryFormatter.Deserialize(memoryStream);
+                memoryStream.Close();
+                memoryStream.Dispose();
+            }
+        }
+    }
+}
